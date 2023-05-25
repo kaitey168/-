5995896df07d4f486726100c7071acd35917ebbe
kaitey168 <134349514+kaitey168@users.noreply.github.com>
2023-05-25 12:22:34 +0800

    addons
    addons

---
+++ addons/dplayer/config/config.php
@@ -0,0 +1,63 @@
+<?php
+$player=array (
+  'ads' => 
+  array (
+    'status' => '0',
+    'vip' => '0',
+    'group' => '1',
+  ),
+  'pre' => 
+  array (
+    'status' => '0',
+    'url' => 'https://ae01.alicdn.com/kf/H9eaa65d8fde748a4bdc554a02004c3437.png',
+  ),
+  'logo' => 
+  array (
+    'status' => '0',
+    'url' => 'https://ae01.alicdn.com/kf/H8b27efd72b3943a9af5d65f58644d255K.png',
+  ),
+  'copyright' => 
+  array (
+    'status' => '0',
+    'content' => '麻豆源码',
+    'url' => 'https://madouym.com',
+  ),
+  'dp' => 
+  array (
+    'auto' => '1',
+    'last' => '1',
+    'next' => '1',
+    'h5' => NULL,
+  ),
+);
+$pre=array (
+  'ads' => 
+  array (
+    'status' => '1',
+    'time' => '60',
+    'button' => '1',
+    'auth' => '0',
+    'group' => '3',
+  ),
+  'pic' => 
+  array (
+    'status' => '1',
+    'img' => 'https://ae01.alicdn.com/kf/H9eaa65d8fde748a4bdc554a02004c3437.png',
+    'link' => 'https://madouym.com',
+    'width' => '',
+    'height' => '',
+  ),
+  'vod' => 
+  array (
+    'status' => '0',
+    'url' => '',
+    'link' => '',
+  ),
+);
+$pause=array (
+  'status' => '1',
+  'pic' => 'https://ae01.alicdn.com/kf/H9eaa65d8fde748a4bdc554a02004c3437.png',
+  'width' => '',
+  'height' => '',
+  'link' => 'https://madouym.com',
+);
